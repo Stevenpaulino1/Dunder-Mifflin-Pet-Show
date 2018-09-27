11ad680c4708da603d58d657cd678a8c23da6abb
matt-mcalister <matt.mcalister93@gmail.com>
2018-09-27 12:10:32 -0400

    finished index and show for employees and dogs

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,10 @@
 class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all.to_a
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+    @employees = @dog.employees
+  end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,10 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all.to_a
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+    @dog = @employee.dog
+  end
 end
